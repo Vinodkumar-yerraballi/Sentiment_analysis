7f4e5ce02acf012aaefcec1e1d6211082ab183dc
Vinodkumar Yerraballi <98636972+Vinodkumar-yerraballi@users.noreply.github.com>
2022-07-02 22:00:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-import emoji
 from PIL import Image
 
 
