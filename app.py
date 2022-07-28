87593ea381d2776baef66c7e4023a10361110281
Vinodkumar Yerraballi <98636972+Vinodkumar-yerraballi@users.noreply.github.com>
2022-07-28 15:12:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,24 @@ def main():
     </div>
     """, unsafe_allow_html=True)
   st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+  @st.cache(persist=True, show_spinner=False)
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: 100 cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+set_background('annie-spratt-_VxyFCTEpW8-unsplash.png')
   user=st.text_area("Enter text hear:","Type hear")
   st.button("Predict")
   if len(user)<1:
